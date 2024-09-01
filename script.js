8c7b3be764886a97a84c68182c17051b32d0385b
markochen <chenmarko77@gmail.com>
2024-09-01 22:41:22 +0300

    first comit

---
+++ script.js
@@ -0,0 +1,62 @@
+// let myName = 'marko'
+// myName = 'dima'
+// console.log(myName)
+// const userAnswer1 = Number(promt('Введіть перше число'))
+// const userAnswer2 = Number(promt('Введіть дуге число'))
+
+
+
+
+
+
+const myName = "Marko"
+console.log(myName)
+
+
+const myAge = 12
+console.log(myAge)
+
+
+const isStudent = true
+console.log(isStudent)
+
+const myString = "bomboklad"
+console.log(myString)
+
+
+let myNumber = 4
+myNumber = myNumber + 10
+console.log(myNumber)
+
+const myNull = null
+console.log(myNull)
+
+
+ const userAnswer1 = prompt('ведіти вашe імя')
+ alert(`привіт ${userAnswer1}`);
+
+
+ if (confirm("Press a button") == true) {
+    alert(`thanks for the confirmation`);}
+    else{
+        alert(`Action canceled`)
+    }
+
+    alert(`you want to enter a secret site`)
+    if (confirm("its dangerous. Do you want to continue?") == true) {
+        alert(`thanks for the confirmation`);}
+        else{
+            alert(`Action canceled`)
+        }
+
+
+// const TrueData = true 
+// const FalseData = false
+// const currentYear = 2024
+// const result = currentYear - isStudent
+// const birthYear = 2012
+
+// alert(result == birthYear);
+// const userAnswer1 = prompt('ведіти ваш вік')
+// const userAnswer2 = prompt('ведіти поточний рік')
+// alert(userAnswer2 - userAnswer1)
